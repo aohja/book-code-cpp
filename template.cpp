258b8a736159f11fee8a9e299724febed9d84f92
Aadi Ohja <106124618+aohja@users.noreply.github.com>
2022-07-10 21:17:22 -0700

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++h>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define per(i, a, b) for (int i = a; i >= b; i--)
+using namespace std;
+ 
+typedef long long ll;
+
+int main() 
+{
+
+}
